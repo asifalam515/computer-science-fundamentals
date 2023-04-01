289983bf4c64b9d673452498f490997ba210bd40
asifalam515 <asibulalam515@gmail.com>
2023-04-02 05:17:16 +0600

    file changed

---
+++ C Programming/module1/output.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is Asif\n");
+    printf("i am a student\n");
+    return 0;
+}
---
+++ C Programming/module1/variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int num1=34;
+    float num2=23.23;
+    char fav='A';
+   printf("%f0.2",num2);
+    return 0;
+}

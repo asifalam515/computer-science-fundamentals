c433381df278f4c0e7a035209407ea1b67c6b5be
asifalam515 <asibulalam515@gmail.com>
2023-04-03 13:14:05 +0600

    nested condition added

---
+++ C Programming/module2/conditional_1.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+int main()
+{
+    int tk;
+    printf("Enter Your money: ");
+    scanf("%d",&tk);
+    if(tk>=1000)
+    {
+        
+        printf("I will visit kuakhata with %d tk\n",tk);
+         if (tk>=1500)
+        {
+        printf(" i will visit sajeke");
+        }
+        else
+        {
+            printf("I will back to me");
+        }
+    }
+    else
+    {
+        printf("Will stay at HOme");
+    }
+       
+        
+   
+
+    return 0;
+}

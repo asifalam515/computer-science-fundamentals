27851177d556d9235d2cb990d33b128935d6d5c0
asifalam515 <asibulalam515@gmail.com>
2023-04-05 14:29:47 +0600

    loop updated

---
+++ C Programming/module_3_Loop/output/do_while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    // initalization
+    int i=0;
+    do
+    {
+        printf("%d\n",i);
+        // increamenet or decreament
+        i++;
+    }
+    // condition
+    while (i<=10);
+    
+    return 0;
+
+}
--- C Programming/module_3_Loop/while.c
+++ C Programming/module_3_Loop/while.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    printf("while loopp ");
+    // initialization
+    int i=1;
+    // condition
+    while (i<=5)
+    {
+        printf("%d\n",i);
+        // increament/decremenet
+        i++;
+    }
     return 0;
+    
 }

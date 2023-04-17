5cc6afc9fb55743fc8274ff30701eb25ff39fc34
asifalam515 <asibulalam515@gmail.com>
2023-04-17 21:30:09 +0600

    array operation

---
+++ C Programming/m_9_arrOperation/insertInArr.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+  int arr[n+1];
+  for (int  i = 0; i < n; i++)
+  {
+   scanf("%d",&arr[i]);
+  }
+  
+//   insert arr[2]=100;
+int pos=2;
+int value=100;
+for (int  i = n; i >=pos+1; i--)
+{
+    arr[i]=arr[i-1];
+
+}
+arr[pos]=value;
+for (int i = 0; i <= n; i++)
+{
+    printf("%d ",arr[i]);
+}
+
+
+
+    return 0;
+}

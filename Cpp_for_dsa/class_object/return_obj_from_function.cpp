a7d578ba5b6197b5da5d78186ae52ee20c76e9f7
asifalam515 <asibulalam515@gmail.com>
2023-05-28 09:20:05 +0600

    dynamic object

--- Cpp_for_dsa/class_object/return_obj_from_function.cpp
+++ Cpp_for_dsa/class_object/return_obj_from_function.cpp
@@ -4,28 +4,26 @@ class Student{
     public:
     char name[100];
     int age;
-    int id;
-    int position;
-    Student(char *n,int a,int r,int c)
+    int roll;
+    char section;
+    Student(char *n,int a,int r,char s)
     {
         strcpy(name,n);
         age=a;
-        id=r;
-        position=c;
-
+        roll=r;
+        section=s;
     }
 };
 
 Student fun()
 {
-    char a[100]="asibul alam";
-    Student saif(a,23,34,14);
-    return saif;
+    Student asif("asibul alam",18,24323,'A');
+    return asif;
 }
 int main()
 {
-  Student asif("asibul alam",23,2,7);
-cout<<asif.name<<endl;
+  Student a=fun();
+  cout<<a.age<<endl;
 
     return 0;
 }

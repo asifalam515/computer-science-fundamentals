b55d1f55f1d65ca29743a98f1f59901afacffcac
asifalam515 <asibulalam515@gmail.com>
2023-09-24 01:25:54 +0600

    python added

---
+++ python/intro_to_python/varibale.py
@@ -0,0 +1,15 @@
+age=23;
+interest_rate=2.5;
+name="jhumme jo pathan"
+districk='B baria'
+isSingle=True
+is_sleeping=False;
+
+# print(type(age))
+# print(type(interest_rate))
+# print(type(districk))
+# print(type(isSingle))
+"""  multiline comment"""
+""" here we go for multiline comment """
+print('asibul'+' '+'alam')
+text=f"kodom ali {age}.Lives in {districk}."

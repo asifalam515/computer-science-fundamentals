7671dbe8e9cfa2978d07c4d645937d65a20d9d5d
asifalam515 <asibulalam515@gmail.com>
2023-12-02 17:10:26 +0600

    module 10 done

--- django/project_3/first_app/views.py
+++ django/project_3/first_app/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home(request):
-    d={'author':'Rahim','age':20}
-    return render(request,'home.html',context=d)
+    d={'author':'Rahim','age':20,'lst':['python','is','best'],'birthday':datetime.datetime.now() ,'val':' ','courses':[
+        {
+            'id':1,
+            'name':'python',
+            'fee':5000
+        },
+        {
+            'id':2,
+            'name':'Django',
+            'fee':10000
+        },
+        {
+            'id':3,
+            'name':'c',
+            'fee':1000
+        }
+        
+    ]}
+    return render(request,'home.html',d)

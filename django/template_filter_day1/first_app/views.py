7042956443fb5d055603fa44ae2b0a4a4744621d
asifalam515 <asibulalam515@gmail.com>
2023-12-12 17:28:12 +0600

    practice day 1 done

--- django/template_filter_day1/first_app/views.py
+++ django/template_filter_day1/first_app/views.py
@@ -11,6 +11,17 @@ def home(request):
         'lst':[12,54,1,5,71,72],
         'txt':['python', 'is', 'fun'],
         'date':datetime.datetime.now(),
+        'str':'',
+        'lstr':' Python coding contest',
+        'framework':'django',
+        'dictsort':[
+    {'name': 'Josh', 'age': 19},
+    {'name': 'Dave', 'age': 22},
+    {'name': 'Joe', 'age': 31},
+   
+    
+   
+],
         
     }
     return render(request,'first_app/index.html',context)

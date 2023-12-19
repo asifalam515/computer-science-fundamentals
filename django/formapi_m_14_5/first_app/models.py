70d59fdf6e62176216435327fa143b1eb0f9dec2
asifalam515 <asibulalam515@gmail.com>
2023-12-19 14:07:04 +0600

    module 14.5 practice day 1 done

--- django/formapi_m_14_5/first_app/models.py
+++ django/formapi_m_14_5/first_app/models.py
@@ -2,8 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    id=models.IntegerField(primary_key=True)
+    # id=models.IntegerField(primary_key=True)
+    id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=20)
     address=models.TextField()
+    # message=models.CharField(max_length=20)
+   
+    email_address=models.EmailField(blank=True,null=True)
+    files=models.BinaryField(blank=True,null=True)
+    isProgrammer=models.BooleanField(blank=True,null=True)
+    birthday=models.DateField(blank=True,null=True) 
+    showTime=models.DateTimeField(blank=True,null=True)
+    showDuration=models.DurationField(blank=True,null=True)
+    image=models.ImageField(blank=True,null=True)
+    settingJson=models.JSONField(blank=True,null=True)
+    ipType=models.GenericIPAddressField(blank=True,null=True)
+    # nullField=models.NullBooleanField()
+    smallInteger=models.SmallIntegerField(blank=True,null=True)
+   
+    
+    
+    
+    
     def __str__(self) -> str:
         return f'Id:{self.id},Name:{self.name}'

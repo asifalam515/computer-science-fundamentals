86bc77c294767f6a4e003029960d0cac376cd1af
asifalam515 <asibulalam515@gmail.com>
2023-12-20 16:44:34 +0600

    module 15 done

--- django/blog_project/posts/urls.py
+++ django/blog_project/posts/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from posts.views import add_post
+from posts.views import edit_post,delete_post
 
 urlpatterns = [
     path('add/', add_post,name='add_post'),
+    path('edit/<int:id>',edit_post,name='edit_post'),
+    path('delete/<int:id>',delete_post,name="delete_post")
 ]
--- django/blog_project/posts/views.py
+++ django/blog_project/posts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect
 from posts.forms import PostForm
-
+from posts.models import Post
 # Create your views here.
 def add_post(request):
     if request.method == 'POST':
-        post_form=PostForm(request.POST)
+        post_form=PostForm(request.POST )
         if  post_form.is_valid():
             post_form.save()
             return redirect('add_post')
@@ -12,3 +12,24 @@ def add_post(request):
         post_form=PostForm()
 
     return render(request,'add_post.html',{'form':post_form})
+
+
+def edit_post(request,id):
+    post=Post.objects.get(pk=id)
+    post_form=PostForm(instance=post)
+    # print(post.tittle)
+    if request.method == 'POST':
+        post_form=PostForm(request.POST,instance=post)
+        if  post_form.is_valid():
+            post_form.save()
+            return redirect('hompage')
+    else:
+        post_form=PostForm(instance=post)
+
+    return render(request,'add_post.html',{'form':post_form})
+
+
+def delete_post(request,id):
+    post=Post.objects.get(pk=id)
+    post.delete()
+    return redirect('hompage')

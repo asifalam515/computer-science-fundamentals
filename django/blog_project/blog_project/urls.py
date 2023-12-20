34f7ed02dd05b086d09af9218edfef7dcfb81eff
asifalam515 <asibulalam515@gmail.com>
2023-12-20 10:16:40 +0600

    category added

--- django/blog_project/blog_project/urls.py
+++ django/blog_project/blog_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='hompage'),
     path('author/',include('author.urls')),
     path('profile/',include('profiles.urls')),
     path('post/',include('posts.urls')),
